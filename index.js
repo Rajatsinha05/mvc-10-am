ee79fbae5a254e19210ed84ef6bd0dce79243d33
Rajat sinha <rajatsinha5467@gmail.com>
2024-08-02 11:22:34 +0530

    mvc

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const dbConnect = require("./config/db")
+const userRouter = require("./routes/user.route")
+const app = express()
+app.use(express.json())
+app.get("/", (req, res) => {
+    res.send("working ")
+})
+
+app.use("/user",userRouter)
+
+app.listen(8090, () => {
+    console.log("listening port 8090");
+   dbConnect()
+})
